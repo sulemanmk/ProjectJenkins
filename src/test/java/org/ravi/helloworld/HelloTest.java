7687e4c7d27a0cfcfbab905828d50cfeef22773c
rakshabn <sulemanmk@gmail.com>
2017-12-26 15:35:55 +0530

    Updating the logger message

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -29,9 +29,13 @@ public class HelloTest {
 
 	@Test
 	public void f() {
+
+		System.out.println(" --- Test f() --- ");
 		text = driver.findElement(By.xpath("html/body/h2"));
+		System.out.println("text: "+text);
+		System.out.println("getTitle: "+driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "");
-		System.out.println("Test Passed");
+		System.out.println(" --- Test Passed --- ");
 
 	}
 
